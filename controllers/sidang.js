8f621f5b715a77231a32d0080832ee696e617fb4
christianHadinata <christianhadinata2004@gmail.com>
2024-12-02 11:40:30 +0700

    fix: fix text query based on role

--- controllers/sidang.js
+++ controllers/sidang.js
@@ -4,42 +4,57 @@ export const getListUserSidangAll = async (req, res) => {
   const { email, role } = req.query;
   console.log(email, role);
 
-  let textQuery = `
-  SELECT 
-    s.idSidang AS "idSidang",
-    s.judulSkripsi AS "judulSkripsi",
-    s.TA AS "TA",
-    m.nama AS "namaMahasiswa",
-    mhs.npm AS "npm",
-    p_dosen.email AS "emailDosen",
-    pms_dosen.role AS "roleDosen"
-FROM 
-    Sidang s
-JOIN 
-    PenggunaMengikutiSidang pms_mahasiswa ON s.idSidang = pms_mahasiswa.idSidang
-JOIN 
-    Mahasiswa mhs ON pms_mahasiswa.email = mhs.email
-JOIN 
-    Pengguna m ON mhs.email = m.email
-JOIN 
-    PenggunaMengikutiSidang pms_dosen ON s.idSidang = pms_dosen.idSidang
-JOIN 
-    Pengguna p_dosen ON pms_dosen.email = p_dosen.email`;
+  let textQuery = "";
 
-  if (role === "Mahasiswa") {
-    textQuery += `
-    WHERE 
-      m.email = $1
-      AND pms_dosen.role != 'Mahasiswa'
-    ORDER BY 
+  if (role !== "Mahasiswa") {
+    textQuery = `
+  SELECT 
+      s.idSidang AS "idSidang",
+      s.judulSkripsi AS "judulSkripsi",
+      s.TA AS "TA",
+      s.tahunAjaran AS "tahunAjaran",
+      m.nama AS "namaMahasiswa",
+      mhs.npm AS "npm",
+      p_dosen.email AS "emailDosen",
+      pms_dosen.role AS "roleDosen"
+  FROM 
+      Sidang s
+  JOIN 
+      PenggunaMengikutiSidang pms_mahasiswa ON s.idSidang = pms_mahasiswa.idSidang
+  JOIN 
+      Mahasiswa mhs ON pms_mahasiswa.email = mhs.email
+  JOIN 
+      Pengguna m ON mhs.email = m.email
+  JOIN 
+      PenggunaMengikutiSidang pms_dosen ON s.idSidang = pms_dosen.idSidang
+  JOIN 
+      Pengguna p_dosen ON pms_dosen.email = p_dosen.email
+  WHERE 
+      p_dosen.email = $1
+      AND pms_mahasiswa.role = 'Mahasiswa'
+      ORDER BY 
       s.idSidang `;
   } else {
-    textQuery += `
-    WHERE 
-    p_dosen.email = $1
-    AND pms_mahasiswa.role = 'Mahasiswa'
-    ORDER BY 
-    s.idSidang `;
+    textQuery = `
+  SELECT 
+      s.idSidang AS "idSidang",
+      s.judulSkripsi AS "judulSkripsi",
+      s.TA AS "TA",
+      s.tahunAjaran AS "tahunAjaran",
+      m.nama AS "namaMahasiswa",
+      mhs.npm AS "npm"
+  FROM 
+      Sidang s
+  JOIN 
+      PenggunaMengikutiSidang pms_mahasiswa ON s.idSidang = pms_mahasiswa.idSidang
+  JOIN 
+      Mahasiswa mhs ON pms_mahasiswa.email = mhs.email
+  JOIN 
+      Pengguna m ON mhs.email = m.email
+  WHERE 
+      m.email = $1
+      ORDER BY 
+        s.idSidang`;
   }
 
   const values = [email];
