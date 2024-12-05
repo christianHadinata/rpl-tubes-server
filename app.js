31f86b29ccdf8e4fea162c4557e2732aa4568366
Josua <josua312@gmail.com>
2024-12-06 00:35:11 +0700

    feat: add koordinator and dosen routes

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 import express from "express";
 const app = express();
 
+// Morgan
+import morgan from "morgan";
+
 // Serve static from public folder
 app.use(express.static("public"));
 
@@ -30,15 +33,22 @@ app.use(express.json());
 //Setting up cors
 app.use(cors());
 
+// Morgan
+app.use(morgan("dev"));
+
 // Middleware import
 
 // Routes import
 import userRoute from "./routes/user.js";
 import sidangRoute from "./routes/sidang.js";
+import koordinatorRoute from "./routes/koordinator.js";
+import dosenRoute from "./routes/dosen.js";
 
 // Routes
 app.use("/api", userRoute);
 app.use("/api/sidang", sidangRoute);
+app.use("/api/koordinator", koordinatorRoute);
+app.use("/api/dosen", dosenRoute);
 
 //Error handling
 app.use(errorHandler);
