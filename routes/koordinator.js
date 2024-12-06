172b235970e3319c5afe34d12234294074ca9034
christianHadinata <christianhadinata2004@gmail.com>
2024-12-07 01:13:12 +0700

    feat: add getKomponenDanBobot and createKomponenDanBobot routes

--- routes/koordinator.js
+++ routes/koordinator.js
@@ -2,11 +2,15 @@ import express from "express";
 import {
   createDataSidang,
   getAllMahasiswa,
+  getKomponenDanBobot,
+  createKomponenDanBobot,
 } from "../controllers/koordinator.js";
 
 const router = express.Router();
 
 router.get("/all-mahasiswa", getAllMahasiswa);
+router.get("/komponen-bobot", getKomponenDanBobot);
 router.post("/tambah-data-sidang", createDataSidang);
+router.post("/komponen-bobot", createKomponenDanBobot);
 
 export default router;
