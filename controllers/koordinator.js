e7fc800313aa5553ead5e3ada11a9ea692055206
unknown <andrewaruan@gmail.com>
2024-12-08 16:08:22 +0700

    feat: add createNilai koordinator controllers

--- controllers/koordinator.js
+++ controllers/koordinator.js
@@ -148,6 +148,8 @@ LEFT JOIN
     KomponenNilai kn
 ON 
     pn.role = kn.role
+WHERE
+    kn.isActive = TRUE
 GROUP BY 
     pn.role, pn.persentase`;
 
@@ -192,7 +194,21 @@ export const createKomponenDanBobot = async (req, res) => {
 
     await client.query(textQueryPersentase, valuesPersentase);
 
-    // Masukin ke tabel KomponenNilai
+    // di komponenNilai isActive nya jadiin false dulu yg dulu
+    const textQueryUpdateIsActive = `
+    UPDATE
+      KomponenNilai
+    SET
+      isActive = FALSE
+    WHERE
+      role = $1
+    `;
+
+    const valuesUpdateIsActive = [selectedRole];
+
+    await client.query(textQueryUpdateIsActive, valuesUpdateIsActive);
+
+    // Masukin yg baru ke tabel KomponenNilai
 
     const textQueryKomponenNilai = `
     INSERT INTO
@@ -205,10 +221,223 @@ export const createKomponenDanBobot = async (req, res) => {
       const valuesKomponenNilai = [
         arrNamaKomponen[i],
         selectedRole,
-        arrBobotKomponen[i],
+        arrBobotKomponen[i] / 100,
       ];
 
       await client.query(textQueryKomponenNilai, valuesKomponenNilai);
+    }
+
+    await client.query("COMMIT");
+    return res.json({ success: true });
+  } catch (error) {
+    console.log(error);
+    await client.query("ROLLBACK");
+
+    throw new InternalServerError("An unexpected error occurred");
+  } finally {
+    client.release();
+  }
+};
+
+export const createNilai = async (req, res) => {
+  const { idSidang, nilai } = req.body;
+  const idSidangInt = parseInt(idSidang);
+  const nilaiFloat = parseFloat(nilai);
+
+  const client = await pool.connect();
+
+  // ambil idKomponen koordinator
+  const textQueryGetKomponen = `
+  SELECT
+      kn.idKomponen AS "idKomponen"
+  FROM
+      KomponenNilai kn
+  WHERE
+      kn.role = 'Koordinator';
+  `;
+  const queryResultKomponen = await pool.query(textQueryGetKomponen);
+  const idKomponen = queryResultKomponen.rows[0].idKomponen;
+
+  // cek dulu udah ada nilai belum
+  const textQueryGetNilai = `
+  SELECT
+      idKomponen AS "idKomponen"
+  FROM
+      Nilai
+  WHERE
+      idKomponen = $1 AND idSidang = $2;
+  `;
+
+  const valuesGetNilai = [idKomponen, idSidangInt];
+
+  const queryResultGetNilai = await pool.query(
+    textQueryGetNilai,
+    valuesGetNilai
+  );
+
+  const isNilaiExist =
+    queryResultGetNilai.rows[0].idKomponen != undefined ? true : false;
+
+  console.log("Nilai exist:");
+  console.log(isNilaiExist);
+
+  try {
+    await client.query("BEGIN");
+    // kalau belum ada nilai ya tinggal tambah
+    if (!isNilaiExist) {
+      const textQueryAddNilai = `
+      INSERT INTO 
+          Nilai(idKomponen, idSidang, nilaiDiberi)
+      VALUES 
+          ($1, $2, $3);
+      `;
+
+      const valuesAddNilai = [idKomponen, idSidangInt, nilaiFloat];
+
+      await client.query(textQueryAddNilai, valuesAddNilai);
+
+      // terus hitung nilai kali bobotKomponen
+      const textQueryGetNilai = `
+      SELECT 
+          SUM(Nilai.nilaiDiberi * KomponenNilai.bobot) AS "totalNilai"
+      FROM 
+          Nilai
+      INNER JOIN 
+          KomponenNilai 
+      ON 
+          Nilai.idKomponen = KomponenNilai.idKomponen
+      WHERE
+          Nilai.idKomponen = $1;
+      `;
+
+      const valuesGetNilai = [idKomponen];
+
+      const queryResultGetNilai = await client.query(
+        textQueryGetNilai,
+        valuesGetNilai
+      );
+
+      const nilaiKaliBobot = queryResultGetNilai.rows[0].totalNilai;
+
+      // terus ambil persentase nilai koordinator
+
+      const textQueryPersentaseNilai = `
+      SELECT 
+          persentase
+      FROM
+          PersentaseNilai
+      WHERE
+          role = 'Koordinator'
+      `;
+
+      const queryResultPersentaseNilai = await client.query(
+        textQueryPersentaseNilai
+      );
+
+      const persentase = queryResultPersentaseNilai.rows[0].persentase;
+
+      // hitung nilaiAkhir
+      const nilaiAkhir = (nilaiKaliBobot * persentase) / 100;
+
+      // bikin row NilaiDiBAP
+
+      const textQueryAddNilaiDiBAP = `
+      INSERT INTO
+          NilaiDiBAP(idSidang, role, persentase, nilai, nilaiAkhir)
+      VALUES
+          ($1, $2, $3, $4, $5);
+      `;
+
+      const valuesAddNilaiDiBAP = [
+        idSidangInt,
+        "Koordinator",
+        persentase,
+        nilaiKaliBobot,
+        nilaiAkhir,
+      ];
+
+      await client.query(textQueryAddNilaiDiBAP, valuesAddNilaiDiBAP);
+    } else {
+      // kalau udah ada nilai, di update
+      const textQueryUpdateNilai = `
+      UPDATE 
+          Nilai
+      SET
+          nilaiDiberi = $1
+      WHERE
+          idKomponen = $2 AND idSidang = $3
+      `;
+
+      const valuesUpdateNilai = [nilaiFloat, idKomponen, idSidangInt];
+
+      await client.query(textQueryUpdateNilai, valuesUpdateNilai);
+
+      // terus hitung nilai kali bobotKomponen
+      const textQueryGetNilai = `
+      SELECT 
+          SUM(Nilai.nilaiDiberi * KomponenNilai.bobot) AS "totalNilai"
+      FROM 
+          Nilai
+      INNER JOIN 
+          KomponenNilai 
+      ON 
+          Nilai.idKomponen = KomponenNilai.idKomponen
+      WHERE
+          Nilai.idKomponen = $1;
+      `;
+
+      const valuesGetNilai = [idKomponen];
+
+      const queryResultGetNilai = await client.query(
+        textQueryGetNilai,
+        valuesGetNilai
+      );
+
+      const nilaiKaliBobot = queryResultGetNilai.rows[0].totalNilai;
+
+      // terus ambil persentase nilai koordinator
+
+      const textQueryPersentaseNilai = `
+      SELECT 
+          persentase
+      FROM
+          NilaiDiBAP
+      WHERE
+          idSidang = $1 AND role = $2
+      `;
+
+      const valuesQueryPersentaseNilai = [idSidangInt, "Koordinator"];
+
+      const queryResultPersentaseNilai = await client.query(
+        textQueryPersentaseNilai,
+        valuesQueryPersentaseNilai
+      );
+
+      const persentase = queryResultPersentaseNilai.rows[0].persentase;
+
+      // hitung nilaiAkhir
+      const nilaiAkhir = (nilaiKaliBobot * persentase) / 100;
+
+      // update row NilaiDiBAP
+
+      const textQueryAddNilaiDiBAP = `
+      UPDATE
+          NilaiDiBAP
+      SET
+          nilai = $1,
+          nilaiAkhir = $2
+      WHERE
+          idSidang = $3 AND role = $4
+      `;
+
+      const valuesAddNilaiDiBAP = [
+        nilaiKaliBobot,
+        nilaiAkhir,
+        idSidangInt,
+        "Koordinator",
+      ];
+
+      await client.query(textQueryAddNilaiDiBAP, valuesAddNilaiDiBAP);
     }
 
     await client.query("COMMIT");
