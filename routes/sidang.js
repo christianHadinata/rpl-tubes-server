8b0d18fb2b629a82d2c9e676d523b7c885fa6f78
unknown <andrewaruan@gmail.com>
2024-12-08 16:10:53 +0700

    feat: add catatan sidang routes

--- routes/sidang.js
+++ routes/sidang.js
@@ -2,11 +2,15 @@ import express from "express";
 import {
   getListUserSidangAll,
   getSingleSidang,
+  getCatatanSidang,
+  updateCatatanSidang,
 } from "../controllers/sidang.js";
 
 const router = express.Router();
 
 router.get("/listSidang", getListUserSidangAll);
 router.get("/singleSidang", getSingleSidang);
+router.get("/catatanSidang", getCatatanSidang);
+router.patch("/catatanSidang", updateCatatanSidang);
 
 export default router;
