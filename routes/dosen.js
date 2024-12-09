ec6b6a294d2cf46e733e065b132d1df93717e53b
afifah41 <afifah41@gmail.com>
2024-12-09 21:31:27 +0700

    feat: add tambah nilai routes

--- routes/dosen.js
+++ routes/dosen.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAllDosen } from "../controllers/dosen.js";
+import { getAllDosen, createNilai } from "../controllers/dosen.js";
 
 const router = express.Router();
 
 router.get("/all", getAllDosen);
+router.post("/tambah-nilai", createNilai);
 
 export default router;
