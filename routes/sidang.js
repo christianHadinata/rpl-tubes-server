5e7d31e2a45aeaa86d350b80089a8c776c75ebe3
afifah41 <afifah41@gmail.com>
2024-12-09 21:31:56 +0700

    feat: add update jadwal dan tempat sidang routes

--- routes/sidang.js
+++ routes/sidang.js
@@ -3,7 +3,9 @@ import {
   getListUserSidangAll,
   getSingleSidang,
   getCatatanSidang,
+  getAllKomponenRole,
   updateCatatanSidang,
+  updateJadwalDanTempatSidang,
 } from "../controllers/sidang.js";
 
 const router = express.Router();
@@ -11,6 +13,8 @@ const router = express.Router();
 router.get("/listSidang", getListUserSidangAll);
 router.get("/singleSidang", getSingleSidang);
 router.get("/catatanSidang", getCatatanSidang);
+router.get("/allKomponenRole", getAllKomponenRole);
 router.patch("/catatanSidang", updateCatatanSidang);
+router.patch("/jadwalDanTempatSidang", updateJadwalDanTempatSidang);
 
 export default router;
