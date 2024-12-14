8f4d6ec8ec35a730880a95ad9f424bf90426940e
YohanLeonardo <yohanleonardo150@gmail.com>
2024-12-14 16:08:11 +0700

    feat: add authentication for koordinator routes

--- routes/koordinator.js
+++ routes/koordinator.js
@@ -6,13 +6,26 @@ import {
   createKomponenDanBobot,
   createNilai,
 } from "../controllers/koordinator.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/all-mahasiswa", getAllMahasiswa);
-router.get("/komponen-bobot", getKomponenDanBobot);
-router.post("/tambah-data-sidang", createDataSidang);
-router.post("/komponen-bobot", createKomponenDanBobot);
-router.post("/tambah-nilai", createNilai);
+router.get(
+  "/komponen-bobot",
+  authMiddleware(["Koordinator"]),
+  getKomponenDanBobot
+);
+router.post(
+  "/tambah-data-sidang",
+  authMiddleware(["Koordinator"]),
+  createDataSidang
+);
+router.post(
+  "/komponen-bobot",
+  authMiddleware(["Koordinator"]),
+  createKomponenDanBobot
+);
+router.post("/tambah-nilai", authMiddleware(["Koordinator"]), createNilai);
 
 export default router;
