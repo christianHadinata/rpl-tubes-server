229bab7018b4e885235415510be39e6c093de981
afifah41 <afifah41@gmail.com>
2024-12-14 16:35:49 +0700

    feat: add authentication

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { register, login } from "../controllers/user.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/register", register);
+router.post("/register", authMiddleware(["Admin"]), register);
 router.post("/login", login);
 
 export default router;
